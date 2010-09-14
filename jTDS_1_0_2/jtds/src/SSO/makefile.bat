a2740cfc0108fcd0739a94cdd68dab9f3dc3cd7f
ickzon <ickzon@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2010-09-14 06:48:08 +0000

    o Cleanup after CVS->SVN migration.

--- jTDS_1_0_2/jtds/src/SSO/makefile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_INCLUDES=;%JAVA_HOME%\include;%JAVA_HOME%\include\win32
-set INCLUDE=%INCLUDE%;%JAVA_INCLUDES%
-if exist ".\Release/" del /Q .\Release\*.*
-NMAKE /f "ntlmauth.mak" CFG="ntlmauth - Win32 Release"
-
