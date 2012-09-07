5b9f53af8e42863e5ac1e0e5473142b95dcdca67
ickzon <ickzon@97a8069c-bbaa-4fa2-9e49-561d8e0b19ef>
2012-09-07 13:16:25 +0000

    o Fixed jTDS version reported by the driver.

--- jTDS 1.2 (stable)/src/main/net/sourceforge/jtds/jdbc/Driver.java
+++ jTDS 1.2 (stable)/src/main/net/sourceforge/jtds/jdbc/Driver.java
@@ -55,7 +55,7 @@ public class Driver implements java.sql.Driver {
     /** Driver minor version. */
     static final int MINOR_VERSION = 2;
     /** Driver version miscellanea (e.g "-rc2", ".1" or <code>null</code>). */
-    static final String MISC_VERSION = ".5";
+    static final String MISC_VERSION = ".6";
     /** Set if the JDBC specification to implement is 3.0 or greater. */
     public static final boolean JDBC3 =
             "1.4".compareTo(System.getProperty("java.specification.version")) <= 0;
